c4d01e584b4592a8d3d6edb331efbdb7bb379d0d
OjasWadhwani <ojaswadhwani098@gmail.com>
2024-03-27 22:54:33 +0530

    add Hasura configuration with Neon

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="tooltip">
+        <span className="tooltiptext">I hope I don't get sued for this logo</span>
         <img src={neon} className="Neon-logo" alt="neon" />
+      </div>
       </header>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_HASURA_GRAPHQL_ENDPOINT,
+  cache: new InMemoryCache(),
+  headers: {
+    "x-hasura-admin-secret": process.env.REACT_APP_HASURA_ADMIN_SECRET,
+  },
+});
+
+client
+  .query({
+    query: gql`
+      query TestQuery {
+        files(where: {type: {_eq: "resume"}}) {
+          file_path
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result.data.files[0].file_path));
+
 
 ReactDOM.render(
   <React.StrictMode>
